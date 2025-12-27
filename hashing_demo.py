7066f6fa8745aa20d2bdb0a6b915ccd07d6fc513
Mummadi Surya Tejeswar Reddy <mummadisuryateja@gmail.com>
2025-12-27 06:17:57 +0530

    Update comments and formatting in hashing_demo.py

--- hashing_demo.py
+++ hashing_demo.py
@@ -1,10 +1,9 @@
-"""
-File Name   : hashing_demo.py
-Author      : Surys Teja
-Description : This program demonstrates how Python uses hashing
-              and how elements are mapped into a limited number
-              of positions using modulo operation.
-"""
+
+# File Name   : hashing_demo.py
+# Author      : Surys Teja
+# Description : This program demonstrates how Python uses hashing
+#               and how elements are mapped into a limited number
+#               of positions using modulo operation.
 
 # Creating a set of integers
 s = {1, 4, 10, 25, 67}
@@ -23,25 +22,26 @@ for num in s:
     print(f"Mapped Index (hash % {len(s)}): {index_position}")
     print("-" * 40)
 
-Set: {1, 4, 10, 25, 67}
-Size: 5
+#outut
+#Set: {1, 4, 10, 25, 67}
+#Size: 5
 
-Number: 1
-Hash: 1
-Index: 1
+# Number: 1
+# Hash: 1
+# Index: 1
 
-Number: 4
-Hash: 4
-Index: 4
+# Number: 4
+# Hash: 4
+# Index: 4
 
-Number: 10
-Hash: 10
-Index: 0
+# Number: 10
+# Hash: 10
+# Index: 0
 
-Number: 25
-Hash: 25
-Index: 0
+# Number: 25
+# Hash: 25
+# Index: 0
 
-Number: 67
-Hash: 67
-Index: 2
+# Number: 67
+# Hash: 67
+# Index: 2
