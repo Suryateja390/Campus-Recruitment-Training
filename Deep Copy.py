d0fb2d87ae6938fee44bf83668a560535cf7f7cb
Mummadi Surya Tejeswar Reddy <mummadisuryateja@gmail.com>
2025-12-27 06:10:28 +0530

    Update comments and formatting in Deep Copy.py

---
+++ Deep Copy.py
@@ -0,0 +1,19 @@
+
+#Description
+
+#Deep copy creates independent memory for nested elements.
+
+Code
+import copy
+
+s = [1, 2, [300, 400]]
+s2 = copy.deepcopy(s)
+
+s[2][0] = 500
+
+print("Original:", s)
+print("Copy    :", s2)
+
+#Output
+#Original: [1, 2, [500, 400]]
+#Copy    : [1, 2, [300, 400]]
