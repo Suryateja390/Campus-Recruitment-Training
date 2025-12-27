f4a6efbc2ad4435cdbe5b4ba263941f0796bf7b5
Mummadi Surya Tejeswar Reddy <mummadisuryateja@gmail.com>
2025-12-27 06:13:16 +0530

    Refactor comments in Second Largest Element.py
    Updated comments for clarity and removed redundancy.

---
+++ Second Largest Element.py
@@ -0,0 +1,20 @@
+#Description
+
+#Finds the second largest element in a list.
+
+Code
+s = [1, 2, 3, 4, 3, 2, 4, 5]
+
+first = second = float('-inf')
+
+for num in s:
+    if num > first:
+        second = first
+        first = num
+    elif first > num > second:
+        second = num
+
+print("Second Largest:", second)
+
+#Output
+#Second Largest: 4
